05a8801484d02b8afe87012030f3ffef0185ad88
DAMEK86 <rehn.andreas86@gmail.com>
2015-10-02 21:50:06 +0200

    Project Dokumentation added

---
+++ 3 Dokumentation/make.bat
@@ -0,0 +1,41 @@
+@echo off
+set F=Thesis
+set TEX=pdflatex -quiet
+set BIBTEX=bibtex %F% -quiet
+set BUILDTEX=%TEX% -synctex=1 -interaction=nonstopmode %F%.tex
+set INDEX=makeindex %F%.idx
+
+goto clean-all
+goto all
+goto clean
+
+:clean-all
+	echo - removing old files
+	del /s /f *.ps *.dvi *.aux *.toc *.idx *.ind *.ilg *.log *.out *.brf *.blg *.bbl *.lof *.lol *.lot *.gz %F%.pdf    
+
+:all
+	echo - pdflatex plods...
+    	%BUILDTEX%
+	echo - pdflatex plods...					
+	%BUILDTEX%
+	echo - bibtex plods...	
+	%BIBTEX%
+	echo - bibtex plods...	
+	%BIBTEX%
+	echo - makeindex plods...
+	%INDEX%
+    echo - pdflatex plods... 
+	%BUILDTEX%
+    echo - pdflatex plods... 
+	%BUILDTEX%
+
+:clean
+	echo - removing temp files
+    del /s /f *.ps *.dvi *.aux *.toc *.idx *.ind *.ilg *.log *.out *.brf *.blg *.bbl *.lof *.lol *.lot *.gz
+:check	
+	set /p o=Open PDF? [yn]
+	if "%o%"=="n" goto END
+	if "%o%"=="y" goto OPEN
+:OPEN
+	start %F%.pdf
+:END
